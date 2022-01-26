411cb26d29fddd4b385ef7885860c625fffbc1d0
harsel <tigrejdvz@gmail.com>
2022-01-26 12:07:42 -0500

    Primer commit

---
+++ src/graficos/Lineas.java
@@ -0,0 +1,30 @@
+package graficos;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Lineas extends JPanel{
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.GREEN);
+		g.drawLine(50, 50, 500, 200);
+	}
+
+	public static void main(String[] args) {
+		
+		JFrame ventana = new JFrame("Dibujo de líneas");		
+		Lineas instanciaDeLineas = new Lineas();
+		instanciaDeLineas.setBackground(Color.LIGHT_GRAY);
+		ventana .add(instanciaDeLineas);
+		ventana.setSize(800, 500);
+		ventana.setVisible(true);
+		ventana.setLocationRelativeTo(null);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
